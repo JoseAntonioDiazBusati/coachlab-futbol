5d1033cfcc73429e9a6dcf119f735bb18b0061ab
JoseAntonio <jdiabus511@g.educaand.es>
2026-05-05 11:50:10 +0200

    create component hero

---
+++ frontend/src/app/components/hero/hero.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero',
+  standalone: true,
+  imports: [],
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.scss',
+})
+export class HeroComponent {
+  onStartFree() {
+    // TODO: Implementar lógica para iniciar gratis
+  }
+
+  onShowHow() {
+    // TODO: Implementar scroll a sección de características
+  }
+}
+
