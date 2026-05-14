e088415ef1d1ff9b41cb8d1cd3da1c528eaa9fc5
JoseAntonio <jdiabus511@g.educaand.es>
2026-05-14 16:33:02 +0200

    update app config and routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { DashboardPageComponent } from './components/dashboard/dashboard-page.component';
+import { PlantillaPageComponent } from './components/plantilla-page/plantilla-page.component';
+import { LigaPageComponent } from './components/liga-page/liga-page.component';
 import { authGuard } from './services/auth.guard';
 
 export const routes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'dashboard', component: DashboardPageComponent, canActivate: [authGuard] },
+  { path: 'plantilla', component: PlantillaPageComponent, canActivate: [authGuard] },
+  { path: 'ligas', component: LigaPageComponent, canActivate: [authGuard] },
   { path: '**', redirectTo: '' },
 ];
